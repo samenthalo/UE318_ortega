590dbc48799fea4a45ec823c3caada31d4246668
samenthalo <124697184+samenthalo@users.noreply.github.com>
2025-03-29 18:28:12 +0000

    Add files via upload

---
+++ BiographieController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BiographieController extends Controller
+{
+    public function show($id)
+    {
+        // Logic to show biography
+    }
+
+    public function store(Request $request, $id)
+    {
+        // Logic to store biography
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update biography
+    }
+}
+
